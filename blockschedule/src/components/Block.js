8b70c0713cddd4c1b79566f4935d094ea3ca4e82
SonavAgarwal <42980809+SonavAgarwal@users.noreply.github.com>
2021-10-09 00:26:04 -0700

    fix color on dark tags

--- blockschedule/src/components/Block.js
+++ blockschedule/src/components/Block.js
@@ -99,6 +99,16 @@ function Block(props) {
         [props.block]
     );
 
+    // https://stackoverflow.com/questions/3942878/how-to-decide-font-color-in-white-or-black-depending-on-background-color
+    function pickTextColorBasedOnBgColorSimple(bgColor) {
+        var color = bgColor.charAt(0) === "#" ? bgColor.substring(1, 7) : bgColor;
+        var r = parseInt(color.substring(0, 2), 16); // hexToR
+        var g = parseInt(color.substring(2, 4), 16); // hexToG
+        var b = parseInt(color.substring(4, 6), 16); // hexToB
+        let resp = r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "black" : "white";
+        return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "black" : "white";
+    }
+
     function openMenu() {
         setMenuOpen(true);
     }
@@ -168,6 +178,7 @@ function Block(props) {
                             if (tagObject.link) {
                                 return (
                                     <span
+                                        onMouseDown={(e) => e.stopPropagation()}
                                         onClick={(event) => {
                                             handleTagClick(event, tagObject);
                                         }}
@@ -189,6 +200,7 @@ function Block(props) {
                                     className='roundSpan'
                                     style={{
                                         backgroundColor: tagObject.color,
+                                        color: pickTextColorBasedOnBgColorSimple(tagObject.color),
                                     }}>
                                     {tagObject.text}
                                 </span>
